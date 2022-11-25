8ebae9a88d30d6e676eb4140ae994543c43263b9
Tim Gallant <tim@lilt.com>
2022-11-25 10:47:52 -0800

    cleanup test scripts

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NAME=${1-test-all}
-
-emacs -batch -l ert -l cl -l $NAME.el -f ert-run-tests-batch-and-exit
+cd "src/$1"
+emacs -batch -l ert -l cl -l $1.el -f ert-run-tests-batch-and-exit
+cd -
